8cfe36f40ac6bd41290a4786ab20f89254b2c3e8
yashnerkar <yashnerkar512@gmail.com>
2023-01-04 20:03:56 +0530

    mongodb connection changed

--- app.js
+++ app.js
@@ -7,24 +7,19 @@ const path = require("path");
 const cors = require("cors");
 // dotenv.config({ path: __dirname + '/.env' });
 const url = process.env.DATABASE_URL;
-// console.log(url); //
-// const port = process.env.PORT || 8000;
-const bodyParser = require("body-parser");
-
-try {
-    mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to database");
-} catch (error) {
-    console.log(error);
+const PORT = process.env.PORT || 8000
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
-
 app.use(express.json());
 app.use(cors());
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(require(path.join(__dirname, "routes/user.js")));
 
@@ -39,7 +34,13 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
+app.all('*', (req, res) => {
+    res.json({ "every thing": "is awesome" })
+})
 
-app.listen(process.env.PORT || 8000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
